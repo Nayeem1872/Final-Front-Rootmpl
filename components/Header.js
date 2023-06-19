4f09b2c3962265c9749cd55f6046d1a15d2d550e
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-06-19 08:33:20 +0600

    first commit

---
+++ components/Header.js
@@ -0,0 +1,124 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+import Center from "./Center";
+import { useContext, useState } from "react";
+import { CartContext } from "./CartContext";
+import BarsIcon from "./icons/Bars";
+import SearchIcon from "@/components/icons/SearchIcon";
+
+const StyleHeader = styled.header`
+background-color: #222;
+`;
+
+const Logo = styled(Link)`
+
+color:#fff;
+  text-decoration:none;
+  position: relative;
+  z-index: 3
+
+`;
+
+const Wrapper = styled.div`
+display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+
+`;
+const StyledNav = styled.nav`
+${props => props.mobileNavActive ? `
+    display: block;
+  ` : `
+    display: none;
+  `}
+  gap: 15px;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 70px 20px 20px;
+  background-color: #222;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    position: static;
+    padding: 0;
+  }
+`;
+const NavLink = styled(Link)`
+display: block;
+  color:#aaa;
+  text-decoration:none;
+  padding: 10px 0;
+  @media screen and (min-width: 768px) {
+    padding:0;
+  }
+`;
+const NavButton = styled.button`
+background-color: transparent;
+  width: 30px;
+  height: 30px;
+  border:0;
+  color: white;
+  cursor: pointer;
+  position: relative;
+  z-index: 3;
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+const SideIcons = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    display: inline-block;
+    min-width: 20px;
+    color: white;
+    svg {
+      width: 18px;
+      height: 18px;
+    }
+  }
+`;
+
+
+
+
+export default function Header(){
+  const { cartProducts } = useContext(CartContext);
+  const [mobileNavActive, setMobileNavActive] = useState(false);
+
+
+    return (
+        <StyleHeader>
+            < Center>
+            
+            
+            <Wrapper>
+
+
+           
+            
+         
+        <Logo href={'/'} >Root Bangladesh</Logo>
+        <StyledNav  mobileNavActive={mobileNavActive}>
+        <NavLink href={'/'} >Home</NavLink>
+        <NavLink href={'/categories'} >Product List</NavLink>
+        <NavLink href={'/certificate'} >Certificates</NavLink>
+        <NavLink href={'/cart'} >Cart({cartProducts.length})</NavLink>
+
+        </StyledNav>
+        <SideIcons>
+            <Link href={"/search"}>
+              <SearchIcon />
+            </Link>
+            <NavButton onClick={() => setMobileNavActive((prev) => !prev)}>
+              <BarsIcon />
+            </NavButton>
+          </SideIcons>
+        </Wrapper>
+        </Center>
+        </StyleHeader>
+    )
+}
